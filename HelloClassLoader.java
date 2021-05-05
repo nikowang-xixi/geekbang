206aa3755f1eb2238f5db2a848ed2091d2206f7b
nikowang <505513960@qq.com>
2021-05-05 13:48:18 +0800

    classLoad first

--- HelloClassLoader.java
+++ HelloClassLoader.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -7,14 +8,15 @@ public class HelloClassLoader extends ClassLoader{
     public static void main(String[] args) throws Exception{
         Class zz = new HelloClassLoader().findClass("Hello");
         Object newInstance = zz.newInstance();
-        zz.getMethod("hello").invoke(newInstance);
+        Method[] methods = zz.getMethods();
+        methods[0].invoke(newInstance);
     }
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
         byte[] bytes = new byte[0];
         try {
-            bytes = Files.readAllBytes(Paths.get("E:\\Hello.xlass"));
+            bytes = Files.readAllBytes(Paths.get("E:\\应用软件\\极客学习\\作业相关\\作业相关\\Hello\\Hello.xlass"));
         } catch (IOException e) {
             e.printStackTrace();
         }
