3ebf010d89b6bb7fb669e1b77bbda44b9a68b8b1
可爱的小张🤯 <nikowang@cute-zcc.local>
2021-05-16 16:12:40 +0800

    week02

---
+++ HttpclientUtils.java
@@ -0,0 +1,41 @@
+package com.geekbang.study02;
+
+import ch.qos.logback.core.net.SyslogOutputStream;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+public class HttpclientUtils {
+
+    public static CloseableHttpClient httpclient = HttpClients.createDefault();
+
+    public final static void main(String[] args) throws Exception {
+        String url = "http://localhost:8801/";
+        String result = HttpclientUtils.getString(url);
+        System.out.println("result:" + result);
+
+    }
+
+    private static String getString(String url) throws Exception {
+        try {
+            HttpGet httpget = new HttpGet(url);
+
+            System.out.println("Executing request " + httpget.getURI());
+            CloseableHttpResponse response = httpclient.execute(httpget);
+            try {
+                response = httpclient.execute(httpget);
+                System.out.println(response.getStatusLine());
+                HttpEntity entity1 = response.getEntity();
+                httpget.abort();
+                return EntityUtils.toString(entity1, "UTF-8");
+            } finally {
+                response.close();
+            }
+        } finally {
+            httpclient.close();
+        }
+    }
+}
